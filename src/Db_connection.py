7a02f2277268ca8fb80ab1a551d5acdfd5dfa8dc
dbc2201 <divyanshb22@gmail.com>
2020-01-02 21:57:20 +0530

    add suggestive comment

--- src/Db_connection.py
+++ src/Db_connection.py
@@ -2,6 +2,7 @@ class DatabaseConnection:
     import mysql.connector as mc
     conn = mc.connect(host="localhost", user="root", passwd="", database="Management")
     cur = conn.cursor()
+    # you could use try-catch here
     if cur:
         print("Connected Successfully!")
     else:
